2bbf58c7fd4e0c1d2a51fa57d36631059aa1cba6
dbies34 <ddbies@hotmail.com>
2019-02-21 16:48:46 -0800

    added BoardPanel class started on TicTacToe class

--- TicTacToe/src/tictactoe/BoardPanel.java
+++ TicTacToe/src/tictactoe/BoardPanel.java
@@ -15,10 +15,20 @@ import java.awt.event.*;
  */
 public class BoardPanel extends JFrame {
     
-    
+    JButton[][] buttonArray = new JButton[3][3];
     public BoardPanel(){
         // set layout to 3 by 3 grid
         setLayout(new GridLayout(3, 3));
         
+        
+    }
+    
+    public void buildButtonPanel(){
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                buttonArray[i][j] = new JButton();
+                //buttonArray[i][j].set // set font size
+            }
+        }
     }
 }
