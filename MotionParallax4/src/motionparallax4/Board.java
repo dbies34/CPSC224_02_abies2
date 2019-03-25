5b2c407ae6a7dbb5d72592f0402d370788273c19
pseminatore <pseminatore@gmail.com>
2019-03-24 17:58:57 -0700

    Added Mouse Listeners

--- MotionParallax4/src/motionparallax4/Board.java
+++ MotionParallax4/src/motionparallax4/Board.java
@@ -8,6 +8,7 @@ package motionparallax4;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.util.*;
 
 
 /**
@@ -25,15 +26,17 @@ public class Board extends JPanel implements ActionListener{
     private final int DELAY = 25;
     private final int BIRDS_INTIAL_X = BOARD_WIDTH;
     private final int BIRDS_INTIAL_Y = (int) (BOARD_HEIGHT / 10);
-    private final int GRASS_INITIAL_X = -25;
-    private final int GRASS_INITIAL_Y = 450;//(int) (BOARD_HEIGHT / 15);
+    private final int GRASS_INITIAL_X = -100;
+    private final int GRASS_INITIAL_Y = 50;//(int) (BOARD_HEIGHT / 15);
     
-    private final int GRASS_NEG_SCALE_X = 0 - 5;
-    private final int GRASS_POS_SCALE_X = 5;
-    private final int BIRD_NEG_SCALE_X = 0 - 4;
-    private final int BIRD_POS_SCALE_X = 4;
-    private final int MTN_NEG_SCALE_X = 0 - 3;
-    private final int MTN_POS_SCALE_X = 3;
+    private final int GRASS_NEG_SCALE_X = 0 - 6;
+    private final int GRASS_POS_SCALE_X = 6;
+    private final int BIRD_NEG_SCALE_X = 0 - 5;
+    private final int BIRD_POS_SCALE_X = 5;
+    private final int MTNN_NEG_SCALE_X = 0 - 4;
+    private final int MTNN_POS_SCALE_X = 4;
+    private final int MTNF_NEG_SCALE_X = 0 - 3;
+    private final int MTNF_POS_SCALE_X = 3;
     private final int SUN_NEG_SCALE_X = 0 - 2;
     private final int SUN_POS_SCALE_X = 2;
     
@@ -41,13 +44,15 @@ public class Board extends JPanel implements ActionListener{
     private final int GRASS_POS_SCALE_Y = 5;
     private final int BIRD_NEG_SCALE_Y = 0 - 4;
     private final int BIRD_POS_SCALE_Y = 4;
-    private final int MTN_NEG_SCALE_Y = 0 - 3;
-    private final int MTN_POS_SCALE_Y = 3;
+    private final int MTNN_NEG_SCALE_Y = 0 - 3;
+    private final int MTNN_POS_SCALE_Y = 3;
+    private final int MTNF_NEG_SCALE_Y = 0 - 3;
+    private final int MTNF_POS_SCALE_Y = 3;
     private final int SUN_NEG_SCALE_Y = 0 - 2;
     private final int SUN_POS_SCALE_Y = 2;
     
-    
-    private Timer timer;
+    private Color background;
+    private javax.swing.Timer timer;
     private Image birds;
     private Image grass;
     private Image mountainNear;
@@ -59,9 +64,7 @@ public class Board extends JPanel implements ActionListener{
     private int grassY;
     private int birdsX;
     private int birdsY;
-    private int grassX;
-    private int grassY;
-    private int constraintX;
+     private int constraintX;
     private int constraintY;
     private int mountainNearX;
     private int mountainNearY;
@@ -76,7 +79,7 @@ public class Board extends JPanel implements ActionListener{
         setBackground(Color.cyan);
         setPreferredSize(new Dimension(500, 500));
         
-        constraintX = 0;
+        constraintX = -15;
         constraintY = 0;
         birdsX = BIRDS_INTIAL_X;
         birdsY = BIRDS_INTIAL_Y;
@@ -85,20 +88,19 @@ public class Board extends JPanel implements ActionListener{
         sunX = (int) (BOARD_WIDTH / 1.3);
         sunY = (int) (BOARD_HEIGHT / 50);
         
-        grassX = -20;
-        grassY = 50;
         
-        mountainNearX = 0;
+        mountainNearX = 100;
         mountainNearY = 50;
         
-        mountainFarX = 0;
+        mountainFarX = 100;
         mountainFarY = 50;
         
         loadImages();
         
         addMouseMotionListener(new MyMouseListener());
+        addMouseListener(new OtherML());
         
-        timer = new Timer(DELAY, this);
+        timer = new javax.swing.Timer(DELAY, this);
         timer.start();
     }
     
@@ -120,9 +122,9 @@ public class Board extends JPanel implements ActionListener{
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         drawSun(g);
-        drawBirds(g);
         drawMountainFar(g);
         drawMountainNear(g);
+        drawBirds(g);
         drawGrass(g);
     }
     
@@ -135,7 +137,7 @@ public class Board extends JPanel implements ActionListener{
     }
     
     private void drawGrass(Graphics g){
-        g.drawImage(grass, grassX, grassY, BOARD_WIDTH + 50, BOARD_HEIGHT, this);
+        g.drawImage(grass, grassX, grassY, BOARD_WIDTH + 500, BOARD_HEIGHT, this);
     }
     
     private void drawSun(Graphics g){
@@ -150,6 +152,8 @@ public class Board extends JPanel implements ActionListener{
     private class MyMouseListener implements MouseMotionListener{
         @Override
         public void mouseDragged(MouseEvent e){
+            birdsX = e.getX();
+            birdsY = e.getY();
             
         }
         @Override
@@ -161,29 +165,67 @@ public class Board extends JPanel implements ActionListener{
                 sunX = sunX + SUN_POS_SCALE_X;
                 grassX = grassX + GRASS_POS_SCALE_X;
                 birdsX = birdsX + BIRD_POS_SCALE_X;
+                mountainNearX = mountainNearX + MTNN_POS_SCALE_X;
+                mountainFarX = mountainFarX + MTNF_POS_SCALE_X;
                 constraintX++;
             }
-            if (currentX > 250 && constraintX > -40){
+            if (currentX > 250 && constraintX > -60){
                 sunX = sunX + SUN_NEG_SCALE_X;
                 grassX = grassX + GRASS_NEG_SCALE_X;
                 birdsX = birdsX + BIRD_NEG_SCALE_X;
+                mountainNearX = mountainNearX + MTNN_NEG_SCALE_X;
+                mountainFarX = mountainFarX + MTNF_NEG_SCALE_X;
                 constraintX--;
             }
             
-            if (currentY < 250 && constraintY < 10){
+            if (currentY < 250 && constraintY < 20){
                 sunY = sunY + SUN_POS_SCALE_Y;
                 grassY = grassY + GRASS_POS_SCALE_Y;
                 birdsY = birdsY + BIRD_POS_SCALE_Y;
+                mountainNearY = mountainNearY + MTNN_POS_SCALE_Y;
+                mountainFarY = mountainFarY + MTNF_POS_SCALE_Y;
                 constraintY++;
             }
             if (currentY > 250 && constraintY > -5){
                 sunY = sunY + SUN_NEG_SCALE_Y;
                 grassY = grassY+ GRASS_NEG_SCALE_Y;
                 birdsY = birdsY + BIRD_NEG_SCALE_Y;
+                mountainNearY = mountainNearY + MTNN_NEG_SCALE_Y;
+                mountainFarY = mountainFarY + MTNF_NEG_SCALE_Y;
                 constraintY--;
             }
             
             //repaint();
+        }
+    }
+    
+    private class OtherML implements MouseListener{
+        @Override
+        public void mouseClicked(MouseEvent e){
+            Random rand = new Random();
+            float r = rand.nextFloat();
+            float g = rand.nextFloat();
+            float b = rand.nextFloat();
+            Color bg = new Color(r,g,b);
+            background = bg;
+            setBackground(background);
+            
+        }
+        @Override
+        public void mouseExited(MouseEvent e){
+            setBackground(Color.black);
+        }
+        @Override
+        public void mouseEntered(MouseEvent e){
+            setBackground(background);
+        }
+        @Override 
+        public void mousePressed(MouseEvent e){
+            
+        }
+        @Override
+        public void mouseReleased(MouseEvent e){
+            
         }
     }
     
